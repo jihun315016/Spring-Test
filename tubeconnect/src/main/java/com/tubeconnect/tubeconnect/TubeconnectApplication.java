23e12b4606e5ddc71ef839427a2a979e61f67029
jihun <jihun315016@gmail.com>
2024-06-29 21:18:52 +0900

    프로젝트 생성

---
+++ tubeconnect/src/main/java/com/tubeconnect/tubeconnect/TubeconnectApplication.java
@@ -0,0 +1,13 @@
+package com.tubeconnect.tubeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubeconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubeconnectApplication.class, args);
+	}
+
+}
---
+++ tubeconnect/src/test/java/com/tubeconnect/tubeconnect/TubeconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubeconnect.tubeconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubeconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
