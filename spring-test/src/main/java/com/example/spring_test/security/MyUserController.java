9627764880b3367be6f4427245f8b60e98a5d9f6
jihun <jihun315016@gmail.com>
2024-07-12 16:04:24 +0900

    Spring Security 접근 권한 제어

--- spring-test/src/main/java/com/example/spring_test/security/MyUserController.java
+++ spring-test/src/main/java/com/example/spring_test/security/MyUserController.java
@@ -2,6 +2,9 @@ package com.example.spring_test.security;
 
 import java.util.Optional;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,5 +42,24 @@ public class MyUserController {
 
         List<MyUser> list = myUserRepository.findAll();
         return list;
+    }
+
+    // 함수 위에 이런거 쓰면 로그인 여부 검사하는 로직 간편하게 넣기 가능
+    // @PreAuthorize("isAnonymous()")
+    // @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    // @PreAuthorize("isAuthenticated()") // 인증 안 됐는데 요청하면 인증 페이지(/login)로 이동
+    @GetMapping("/my-page")
+    public String myPage(Authentication auth) {       
+        if (auth != null && auth.isAuthenticated()) {
+            // 인증된 사용자에 대한 처리
+            User user = (User)auth.getPrincipal();
+            System.out.println(user.getUsername());     // admin
+            System.out.println(user.getAuthorities());  // [ROLE_ADMIN]
+            return "mypage.html";
+        } else {
+            // 인증되지 않은 사용자에 대한 처리
+            // 인증 후 로그아웃 상태의 경우 auth가 null이 아님
+            return "login.html";
+        }
     }
 }
--- spring-test/src/main/java/com/example/spring_test/security/SecurityConfig.java
+++ spring-test/src/main/java/com/example/spring_test/security/SecurityConfig.java
@@ -2,25 +2,48 @@ package com.example.spring_test.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 // Spring Security 설정을 지정할 수 있는 어노테이션
 @Configuration
 @EnableWebSecurity 
+@EnableMethodSecurity(prePostEnabled = true) // 이거 해야 컨트롤러에서 @PreAuthorize 애너테이션 쓸 수 있음
 public class SecurityConfig {
     @Bean
     PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // CSRF 공격 방지를 위해 랜덤한 토큰 생성
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        // 뭔가 나중에 공부할 것 같은 느낌이니까 일단 넘거갈거
-        httpSecurity.csrf(csrf -> csrf.disable());
+        // CSRF 보안 기능 비활성화
+        // httpSecurity.csrf(csrf -> csrf.disable());
+        
+        // CSRF 보안 기능 활성화
+        // CSRF는 사용자 인증 정보를 이용하여 악의적인 요청을 보내는 공격
+        // 가능 활성화 시 뷰에 두 가지 작업 필요
+        // 1. <form> 태그 내부에 아래 태그 추가하기 
+        // - <input type="hidden" th:name="${_csrf.parameterName}" th:value="${_csrf.token}"> 추가하기
+        // 2. ajax 요청 시에도 CSRF 토큰을 넣어서 요청 보내기
+        httpSecurity.csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository())
+               .ignoringRequestMatchers("/login") // CSRF 기능에서 제외할
+        );
+
 
         // URL 기반 권한 설정
         // 인증 여부에 상관 없이 모든 경로에 대해 접근을 허용한다.
@@ -42,7 +65,7 @@ public class SecurityConfig {
         // 로그아웃 설정
         httpSecurity.logout(logout -> logout
             .logoutUrl("/logout")
-            .logoutSuccessUrl("/")
+            .logoutSuccessUrl("/login")
             .deleteCookies("JSESSIONID") // 로그아웃 시 세션 쿠키를 삭제합니다.
             .invalidateHttpSession(true)); // 로그아웃 시 세션을 무효화합니다.
 
